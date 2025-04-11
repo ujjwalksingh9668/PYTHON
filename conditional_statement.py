3131850c9738416302c6cb1fefb1864a037109b3
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-04-11 20:08:45 +0530

    Create conditional_statement.py

---
+++ conditional_statement.py
@@ -0,0 +1,16 @@
+# Given an integer,perform the following conditional actions:
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+n = int(input())
+if n % 2:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
