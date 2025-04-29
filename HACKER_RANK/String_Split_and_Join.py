14666b732accc00432959b04582e3e1c40305dc2
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-04-29 11:27:17 +0530

    Create String_Split_and_Join.py

---
+++ HACKER_RANK/String_Split_and_Join.py
@@ -0,0 +1,17 @@
+# Q- In a can split on a
+# = "this •is a string"
+# a = ") # a •is converted to a list of strings.
+# print a
+# ['this' ,
+# ' string 'J
+# a string is Grnple:
+# print a
+# th•i ng
+# Ymi are given a string. Split string a (spæe) defimiter arri join a — hyphen
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+# Solution:-
+a = input()
+a_list = a.split()
+a_joined = "-".join(a_list)
+print(a_joined)
