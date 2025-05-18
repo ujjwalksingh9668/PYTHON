bc8de19bea30c0fa33db6917f81a6f6283d07302
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-05-18 09:22:07 -0700

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,7 @@
 #Given a year, determine whether it is a leap year.
 # If it is a leap year, return the Boolean True, otherwise return False.
 # https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
-
+        
 def is_leap(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
